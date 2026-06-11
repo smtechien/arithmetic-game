e45e3ffd36fb40c54e6fb77d642820d6bf760520
smtechien <sulthonrahmatullah@gmail.com>
2026-06-11 17:18:48 +0800

    fix: title header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,5 +8,6 @@
 <svelte:head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="icon" href={favicon} />
+  <title>Kuis Aritmatika - Smtechien</title>
 </svelte:head>
 {@render children()}
